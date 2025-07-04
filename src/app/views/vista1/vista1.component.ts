b9be4d95f4db74e75ba599bce5950676259c857a
negoxo <josedceballos@gmail.com>
2025-07-03 21:23:56 -0500

    Implementación de vistas y colores

--- src/app/views/vista1/vista1.component.ts
+++ src/app/views/vista1/vista1.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vista1',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './vista1.component.html',
   styleUrl: './vista1.component.css'
 })
